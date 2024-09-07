e9b5929e26383f534541fa4d86be33bcdd0e85f8
Krisztian Lukacs <37411667+krisztianlukacs@users.noreply.github.com>
2024-09-07 16:37:01 +0000

    first rust key-value examples

---
+++ examples/exam/step0.rs
@@ -0,0 +1,23 @@
+use std::collections::HashMap;
+use serde_json::{json, Value};
+
+fn main() {
+    // HashMap creation for key-value pairs storage
+    let mut data_store: HashMap<String, Value> = HashMap::new();
+
+    // key-value pair insertion
+    let key = "user1".to_string();
+    let value = json!({
+        "name": "Chris",
+        "age": 30,
+        "email": "chris@example.com"
+    });
+    data_store.insert(key.clone(), value);
+
+    // key-value pair retrieval
+    if let Some(retrieved_value) = data_store.get(&key) {
+        println!("The {} key value: {}", key, retrieved_value);
+    } else {
+        println!("The {} key not found!", key);
+    }
+}
