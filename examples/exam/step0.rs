ee46ccc8bd6200466a135a6130a8ba8c38ec3613
Krisztian Lukacs <37411667+krisztianlukacs@users.noreply.github.com>
2024-09-08 12:27:28 +0000

    solana balace read

--- examples/exam/step0.rs
+++ examples/exam/step0.rs
@@ -1,25 +1,43 @@
-extern crate serde_json;
+// Working: 2024.09.08
 
 use std::collections::HashMap;
 use serde_json::{json, Value};
 
 fn main() {
+
     // HashMap creation for key-value pairs storage
     let mut data_store: HashMap<String, Value> = HashMap::new();
 
     // key-value pair insertion
     let key = "user1".to_string();
-    let value = json!({
+    
+    // JSON object creation
+    let value: Value = json!({
         "name": "Chris",
         "age": 30,
-        "email": "chris@example.com"
+        "city": "Budapest"
     });
-    data_store.insert(key.clone(), value);
 
-    // key-value pair retrieval
-    if let Some(retrieved_value) = data_store.get(&key) {
+    data_store.insert(key.clone(), value.clone());
+
+    // Print Json data
+    println!("JSON data: {}", value);
+
+    // Acces Json data
+    println!("Name: {}", value["name"]);
+    println!("Age: {}", value["age"]);
+
+    // Check if key exists
+    if let Some(city) = value.get("city") {
+        println!("City: {}", city);
+    } else {
+        println!("The 'city' not found!");
+    }
+
+     // key-value pair retrieval
+     if let Some(retrieved_value) = data_store.get(&key) {
         println!("The {} key value: {}", key, retrieved_value);
     } else {
         println!("The {} key not found!", key);
-    }
+    } 
 }
